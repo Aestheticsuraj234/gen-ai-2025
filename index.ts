cc1faafbb49eabe54cde1471b251bfdc56d96af1
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-12-15 15:25:08 +0530

    Refactor weather agent to include user location tool and enhance system prompt

--- index.ts
+++ index.ts
@@ -1,35 +1,84 @@
 import * as z from "zod";
-import { createAgent, tool } from "langchain";
+import { createAgent, tool, type ToolRuntime } from "langchain";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import dotenv from "dotenv";
+import { MemorySaver } from "@langchain/langgraph";
+
+const checkpointer = new MemorySaver();
 
 dotenv.config();
 
+const systemPrompt = `You are an expert weather forecaster, who speaks in puns.
+
+You have access to two tools:
+
+- get_weather_for_location: use this to get the weather for a specific location
+- get_user_location: use this to get the user's location
+
+If a user asks you for the weather, make sure you know the location. If you can tell from the question that they mean wherever they are, use the get_user_location tool to find their location.`;
+
+
 const getWeather = tool(
-  ({ city }) => `It's always sunny in ${city}!`,
+  (input) => `It's always sunny in ${input.city}!`,
   {
-    name: "get_weather",
+    name: "get_weather_for_location",
     description: "Get the weather for a given city",
     schema: z.object({
-      city: z.string(),
+      city: z.string().describe("The city to get the weather for"),
     }),
   }
 );
+
+type AgentRuntime = ToolRuntime<unknown, { user_id: string }>;
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
   apiKey: process.env.GOOGLE_API_KEY!,
 });
 
+
+const getUserLocation = tool(
+  (_, config: AgentRuntime) => {
+    const { user_id } = config.context;
+    return user_id === "1" ? "Florida" : "SF";
+  },
+  {
+    name: "get_user_location",
+    description: "Retrieve user information based on user ID",
+  }
+);
+
+
+const responseFormat = z.object({
+  punny_response: z.string(),
+  weather_conditions: z.string().optional(),
+});
+
 const agent = createAgent({
-  model, 
-  tools: [getWeather],
-});
+  model,
+  systemPrompt,
+  tools: [getWeather, getUserLocation],
+  responseFormat,
+  checkpointer
 
-const result = await agent.invoke({
-  messages: [
-    { role: "user", content: "What's the weather like in New York?" },
-  ],
-});
+})
 
-console.log(result);
+const config = {
+  configurable: { thread_id: "1" },
+  context: { user_id: "1" },
+};
+
+const response = await agent.invoke(
+  {messages:[{role:"user" , content:"What is the weather outside>"}]},
+  config
+)
+
+console.log(response.structuredResponse);
+
+const thankYouResponse = await agent.invoke(
+  { messages: [{ role: "user", content: "thank you!" }] },
+  config
+);
+
+
+console.log(thankYouResponse.structuredResponse);
